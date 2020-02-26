0d06d232532422b0d45d1cb15d3838a303e0420d
NyQuil <47892094+qbullet@users.noreply.github.com>
2020-02-27 03:59:05 +0700

    Update ResultProvider.js

--- src/providers/ResultProvider.js
+++ src/providers/ResultProvider.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-// const URL = 'http://154.202.3.85/dss/cal.php?api_key=CNL2C8VD&fbclid=IwAR1u7AvWT-M0SPWc4VeEJUjUOkHywyOU9XpOSBjzJNjkZHj84K9GNOWKTrY'
 const URL = 'https://natdanaispace.cf/mining/select_result.php'
 
 
